1ed0991b4a625d6e0b12572e4d074df50d7fbdea
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-10-05 17:09:21 +0700

    removing unnecessary files and install tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+ <div class="home">
+  <h1 class="text-2xl text-yellow-300">Auction Item Lists</h1>
+ </div>
 </template>
